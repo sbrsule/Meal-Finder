a27de7fe258d1e9ff21c53e964a8ca0e4190fda0
sbrsule <sbrsuleiman@gmail.com>
2021-12-03 19:15:28 -0500

    Added composedPath() compatibility

--- script.js
+++ script.js
@@ -90,14 +90,28 @@ function addMealToDOM(meal) {
 submit.addEventListener('submit', searchMeal);
 
 mealsEl.addEventListener('click', e => {
-	const mealInfo = e.path.find(item => {
-		if (item.classList) {
-			return item.classList.contains('meal-info');
-		}
-		else {
-			return false;
-		}
-	});
+	let mealInfo;
+	if (e.path) {
+		mealInfo = e.path.find(item => {
+			if (item.classList) {
+				return item.classList.contains('meal-info');
+			}
+			else {
+				return false;
+			}
+		});
+	}
+	else if (e.composedPath) {
+		mealInfo = e.composedPath().find(item => {
+			if (item.classList) {
+				return item.classList.contains('meal-info');
+			}
+			else {
+				return false;
+			}
+		});
+		console.log(mealInfo);
+	}
 
 	if (mealInfo) {
 		const mealID = mealInfo.getAttribute('data-mealid');
